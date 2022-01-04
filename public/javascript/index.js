90b8aaa5d757cc22fd08c702ffc5905f6ec2bf61
Harshitk002 <hk7017438939@gmail.com>
2022-01-04 13:19:31 +0530

    update url

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = h"ttps://sharekaro-filesharing.herokuapp.com";
+const baseURL = "https://sharekaro-filesharing.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
